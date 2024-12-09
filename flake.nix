a417b9c28166edc9370caeb45b51619e594f9c76
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-08 17:21:51 -0800

    init commit

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
-  description = "Example flake that prints output";
-
   inputs = {
     nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      defaultApp = {
-        type = "app";
-        program = "${pkgs.runtimeShell} -c 'echo Hello, World!'";
-      };
-    });
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        defaultApp = {
+          type = "app";
+          program = "${pkgs.writeShellScript "print-string" ''
+            echo "Hello from Nix!"
+          ''}";
+        };
+      });
 }
